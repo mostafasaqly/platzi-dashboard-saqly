776aabe4b3ab96289398a2c3264bfdc3c6ba4ce2
Mostafa Saqly <mostafasaqly1@gmail.com>
2026-03-07 09:09:54 +0200

    test login with the sample

--- src/app/features/auth/pages/login-page/login-page.ts
+++ src/app/features/auth/pages/login-page/login-page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 import { UI_PRIMENG } from '../../../../shared/ui/ui-primeng';
 import { AppLogo } from '../../../../shared/components/app-logo/app-logo';
 
@@ -8,4 +10,22 @@ import { AppLogo } from '../../../../shared/components/app-logo/app-logo';
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss',
 })
-export class LoginPage {}
+export class LoginPage {
+
+  private http = inject(HttpClient);
+
+  login() {
+    this.http.post('https://api.escuelajs.co/api/v1/auth/login', {
+      email: 'mostafasaqly1@gmail.com',
+      password: 'Passw0rd'
+    }).subscribe({
+      next: (res) => {
+        console.log('Login Success:', res);
+      },
+      error: (err) => {
+        console.error('Login Error:', err);
+      }
+    });
+  }
+
+}
